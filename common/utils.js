24a92bd37779d4434c3067a10ce16d49c307dabc
Rahul Sharma <jagannath@mobifyi.com>
2021-03-23 22:50:48 +0530

    - fixed signature issue

--- common/utils.js
+++ common/utils.js
@@ -1,4 +1,4 @@
-const request = require("request");
+const axios = require("axios");
 const rs = require("jsrsasign");
 const rsu = require("jsrsasign-util");
 const path = require("path");
@@ -17,32 +17,15 @@ module.exports = class UtilService {
      * @returns api call data
      */
     async postWithSignature(endPoint, data, signature) {
-        // const signt = this.getHeaders(signature)
-        // console.log('\n\nsignt', signt)
-        // console.log('endPoint, data, signature', endPoint, data, signt)
-        // return await request.post({
-        //     url: endPoint,
-        //     method: "POST",
-        //     headers: signt,
-        //     body: JSON.stringify(data),
-        // })
-
-        request.post({
+        const signt = this.getHeaders(signature)
+        return await axios.request({
             url: endPoint,
             method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Signature": signature,
-            },
-            body: JSON.stringify(data)
-          }, (err, res, body) => {
-            if(err){
-                console.log('err', err)
-              
-            }else{
-              console.log('------',body)
-            }
-          });
+            data: JSON.stringify(data),
+            headers: signt,
+        })
+
+        
     } // End of postWithSignature function
 
     /**
@@ -57,69 +40,43 @@ module.exports = class UtilService {
         };
     }// End of getHeaders function
 
-    // btoa(str) {
-    //     let buffer;
+    btoa(str) {
+        let buffer;
 
-    //     if (str instanceof Buffer) {
-    //         buffer = str;
-    //     } else {
-    //         buffer = Buffer.from(str.toString(), "binary");
-    //     }
+        if (str instanceof Buffer) {
+            buffer = str;
+        } else {
+            buffer = Buffer.from(str.toString(), "binary");
+        }
 
-    //     return buffer;
-    // } // End of btoa function
+        return buffer;
+    } // End of btoa function
 
-    hexToBase64(str) {
-        return btoa(String.fromCharCode.apply(null,
-        str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" "))
-        );
-    }
 
-    createSignature(certificatePath, data){
-        //Generating Signature start
-        console.log('booooooooommmmm', data);
-        let pem = rsu.readFile('server.key');
-        let sig = new rs.Signature({alg: 'SHA1withRSA'});
-        sig.init(pem);
-        const value1 = JSON.stringify(data);
-        sig.updateString(value1)
-        return sig.sign()
-        // return sigValueHex;
-        //Generating Signature end
-    }
+    hexToBase64(data) {
+        /**
+         * @returns data after converting to base64 using btoa function
+         */
+       return this.btoa(String.fromCharCode.apply(null,
+            data.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" "))
+        ).toString("base64");
+    } // End of hexToBase64 function
 
-    // hexToBase64(data) {
-    //     /**
-    //      * @returns data after converting to base64 using btoa function
-    //      */
-    //     // console.log("data ----->", data);
-    //    return btoa(String.fromCharCode.apply(null,
-    //         data.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" "))
-    //     ).toString("base64");
-    // } // End of hexToBase64 function
+    createSignature(certificatePath, data) {
+        try {
+            let pem = rsu.readFile(certificatePath);
+            const sig = new rs.Signature({alg: "SHA1withRSA"});
+            sig.init(pem);
+            const value1 = JSON.stringify(data);
+            sig.updateString(value1);
 
-    // createSignature(certificatePath, data) {
-    //     try {
-    //         let pem = rsu.readFile(certificatePath);
-    //         // let sig = new rs.Signature({alg: 'SHA1withRSA'});
-    //         // sig.init(pem);
-    //         // const value1 = JSON.stringify(data);
-    //         // sig.updateString(value1)
-    //         // let sigValueHex = sig.sign();
-    //         // return sigValueHex;
-    //         // console.log("Your pem bale bale", pem)
-    //         const sig = new rs.Signature({alg: "SHA1withRSA"});
-    //         sig.init(pem);
-    //         const value1 = JSON.stringify(data);
-    //         sig.updateString(value1);
-
-    //         return sig.sign();
-    //     } catch (error) {
-    //         /**
-    //          * If something went wrong
-    //          */
-    //         // console.error("Error Creating signature", error);
-    //         throw new Error(error);
-    //     }
-    // } // Create RSAwithSHA signature of payload
+            return sig.sign();
+        } catch (error) {
+            /**
+             * If something went wrong
+             */
+            // console.error("Error Creating signature", error);
+            throw new Error(error);
+        }
+    } // Create RSAwithSHA signature of payload
 }
